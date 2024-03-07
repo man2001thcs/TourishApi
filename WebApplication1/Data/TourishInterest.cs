a90d45e69f31ab656e6fd625d6c2cf807aa2705d
EREBUS\dochu <dochu8@gmail.com>
2024-03-07 23:37:39 +0700

    f

--- WebApplication1/Data/TourishInterest.cs
+++ WebApplication1/Data/TourishInterest.cs
@@ -18,10 +18,6 @@ namespace WebApplication1.Data
         public Guid UserId { get; set; }
         public InterestStatus InterestStatus { get; set; }
         public DateTime UpdateDate { get; set; }
-<<<<<<< HEAD
-        public TourishPlan TourishPlan { get; set; }
-=======
->>>>>>> a3c0c39 (Add migration)
         public User User { get; set; }
         public TourishPlan? TourishPlan { get; set; }
     }
