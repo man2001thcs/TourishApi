08b344dd707e7ac8e054b15555f2cf5835fc2265
EREBUS\dochu <dochu8@gmail.com>
2024-03-07 23:34:25 +0700

    add migration

---
+++ WebApplication1/Migrations/20240307093403_addInterestdb.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourishApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addInterestdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TourishInterest",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TourishPlanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InterestStatus = table.Column<int>(type: "int", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TourishInterest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TourishPlan_TourishInterest",
+                        column: x => x.TourishPlanId,
+                        principalTable: "TourishPlan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_User_TourishInterest",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourishInterest_TourishPlanId",
+                table: "TourishInterest",
+                column: "TourishPlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourishInterest_UserId",
+                table: "TourishInterest",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TourishInterest");
+        }
+    }
+}
